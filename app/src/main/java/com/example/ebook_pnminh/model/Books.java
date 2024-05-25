c6e3fc78eb6ba356c6cd554e86a9366f688e9abc
minh <phamnhatminh305203@gmail.com>
2024-05-25 14:50:13 +0700

    add firebase

---
+++ app/src/main/java/com/example/ebook_pnminh/model/Books.java
@@ -0,0 +1,11 @@
+package com.example.ebook_pnminh.model;
+
+import android.net.Uri;
+
+public class Books {
+    private int id;
+    private String name;
+    private Uri image;
+
+
+}
---
+++ app/src/main/java/com/example/ebook_pnminh/model/BooksRepository.java
@@ -0,0 +1,8 @@
+package com.example.ebook_pnminh.model;
+
+import java.util.ArrayList;
+
+public class BooksRepository {
+
+
+}
